0b92184fa73f0266a3036edcfbb1d6a78895ba07
Ariane - ( ˘▽˘)っ♨ <112662431+ariianel@users.noreply.github.com>
2023-04-16 23:42:13 +0200

    Update IGrapheTest.java

--- src/com/GraphesOrientesValues/IGrapheTest.java
+++ src/com/GraphesOrientesValues/IGrapheTest.java
@@ -10,42 +10,51 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class IGrapheTest {
+	private IGraphe[] graphes = {
+			new GrapheLArcs(), new GrapheLArcs(),
+			new GrapheMAdj(),
+			new GrapheHHAdj()
+	};
 	// graphe de l'exercice 3.1 du poly de maths
 	// avec en plus un noeud isole : J
-	private String g31 = 
+	private String g31 =
 			"A-C(2), A-D(1), "
-			+ "B-G(3), "
-			+ "C-H(2), "
-			+ "D-B(3), D-C(5), D-E(3), "
-			+ "E-C(1), E-G(3), E-H(7), "
-			+ "F:, "
-			+ "G-B(2), G-F(1), "
-			+ "H-F(4), H-G(2), "
+					+ "B-G(3), "
+					+ "C-H(2), "
+					+ "D-B(3), D-C(5), D-E(3), "
+					+ "E-C(1), E-G(3), E-H(7), "
+					+ "F:, "
+					+ "G-B(2), G-F(1), "
+					+ "H-F(4), H-G(2), "
+					+ "I-H(10), "
+					+ "J:";
+
+	private String g31a = ""       // arcs non tries
+			+ "D-C(5), D-E(3), D-B(3), "
+			+ "E-G(3), E-C(1), E-H(7), "
 			+ "I-H(10), "
-			+ "J:";
-	
+			+ "J:,"
+			+ "G-B(2), G-F(1), "
+			+ "F:, "
+			+ "H-G(2), H-F(4), "
+			+ "A-C(2), A-D(1), "
+			+ "B-G(3), "
+			+ "C-H(2) ";
+
 	@Test
 	void exo3_1Maths() {
-		//liste d'arc
-		GrapheLArcs gla = new GrapheLArcs(g31);
-		//Hachage
-		GrapheHHAdj glh = new GrapheHHAdj(g31);
-		//matrice adj
-		GrapheMAdj gma = new GrapheMAdj(g31);
-		//Liste d'adjacence
-		GrapheLAdj glad = new GrapheLAdj(g31); 
-		//tests
-		tester3_1(gla);
-		tester3_1(glh);
-		tester3_1(gma);
-		tester3_1(glad);
+		for (IGraphe g : graphes) {
+			g.peupler(g31a);
+			tester3_1(g);
+		}
+
 	}
-	
+
 	void tester3_1(IGraphe g) {
 		List<String> sommets_exp = List.of("A","B","C","D","E","F","G","H","I","J");
 		List<String> sommets = new ArrayList<String>(g.getSommets()); // pas forcement triee
 		Collections.sort(sommets);
-		assertEquals(sommets_exp, g.getSommets());
+		assertEquals(sommets_exp, sommets);
 		assertTrue(g.contientSommet("C"));
 		assertFalse(g.contientSommet("c"));
 		assertTrue(g.contientArc("C","H"));
@@ -55,33 +64,35 @@ class IGrapheTest {
 		Collections.sort(successeurs);
 		assertEquals(List.of("B","C", "E"), successeurs);
 		assertEquals(g31, g.toString());
-		
+
 		g.ajouterSommet("A"); // ne fait rien car A est deja present
 		assertEquals(g31, g.toString());
-		assertThrows(IllegalArgumentException.class,  
-				() -> g.ajouterArc("G", "B", 1));// deja present
+		assertThrows(IllegalArgumentException.class,
+				() -> g.ajouterArc("G", "B", 1));		// deja present
 		g.oterSommet("X"); // ne fait rien si le sommet n'est pas present
 		assertEquals(g31, g.toString());
 		assertThrows(IllegalArgumentException.class,
 				() -> g.oterArc("X", "Y"));  // n'existe pas
-		
+
 		assertThrows(IllegalArgumentException.class,
 				() -> g.ajouterArc("A", "B", -1)); // valuation negative
 	}
-	
-	@Test
-	void importer() throws NumberFormatException, FileNotFoundException {
-		System.out.println("SAE graphes");
-		IGraphe g = new GrapheLArcs();
+
+	void testImportation(IGraphe g) {
 		Arc a = GraphImporter.importer("src/com/GraphesOrientesValues/graphes/ac/g-10-1.txt", g);
-		assertEquals(g.toString(), "1-3(5), "
-				+ "10-3(3), 2-1(5), 2-3(5), 2-5(4), "
-				+ "3-4(4), 3-5(4), 4-10(1), 4-2(1), 4-7(3), "
-				+ "5-9(4), 6-2(3), 6-3(4), 7-3(2),"
-				+ " 8-2(4), 8-6(1), 9-2(4)");
+		assertEquals("1-3(5), "
+						+ "10-3(3), 2-1(5), 2-3(5), 2-5(4), "
+						+ "3-4(4), 3-5(4), 4-10(1), 4-2(1), 4-7(3), "
+						+ "5-9(4), 6-2(3), 6-3(4), 7-3(2),"
+						+ " 8-2(4), 8-6(1), 9-2(4)",
+				g.toString());
 		assertEquals("5", a.getNoeudSource());
 		assertEquals("7", a.getNoeudDest());
 	}
 
+	@Test
+	void importer() throws NumberFormatException, FileNotFoundException {
+		for (IGraphe g : graphes)
+			testImportation(g);
+	}
 }
-
